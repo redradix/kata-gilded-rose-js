6d74832d6332f104c91e9c8747ca9ce1b8f05292
Victor Miguez <victor@redradix.com>
2021-05-07 16:15:50 +0200

    Extracted kind of constant

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -18,59 +18,65 @@ var GildedRose = function () {
   updateQuality(items)
 }
 
+const SPECIAL_QUALITY_ITEMS = [AGED_BRIE, BACKSTAGE, SULFURAS]
+
 export function updateQuality(items) {
   for (var i = 0; i < items.length; i++) {
-    if (AGED_BRIE !== items[i].name && BACKSTAGE !== items[i].name && items[i].quality > 0 && SULFURAS !== items[i].name) {
-      items[i].quality = items[i].quality - 1
-    } else {
-      if (items[i].quality < 50) {
+    const item = items[i]
 
-        items[i].quality = items[i].quality + 1
+    if (!SPECIAL_QUALITY_ITEMS.includes(item.name) && item.quality > 0) {
+      item.quality = item.quality - 1
+    }
 
-        if (AGED_BRIE === items[i].name && items[i].sellIn < 6) {
-          items[i].quality = items[i].quality + 1
+    if (SPECIAL_QUALITY_ITEMS.includes(item.name)) {
+      if (item.quality < 50) {
+
+        item.quality = item.quality + 1
+
+        if (AGED_BRIE === item.name && item.sellIn < 6) {
+          item.quality = item.quality + 1
         }
 
-        if (AGED_BRIE === items[i].name && items[i].sellIn < 11) {
-          items[i].quality = items[i].quality + 1
+        if (AGED_BRIE === item.name && item.sellIn < 11) {
+          item.quality = item.quality + 1
         }
 
-        if (BACKSTAGE === items[i].name) {
-          if (items[i].sellIn < 11 && items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1
+        if (BACKSTAGE === item.name) {
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality = item.quality + 1
           }
 
-          if (items[i].sellIn < 6 && items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality = item.quality + 1
           }
         }
       }
     }
 
-    if (SULFURAS !== items[i].name) {
-      items[i].sellIn = items[i].sellIn - 1
+    if (SULFURAS !== item.name) {
+      item.sellIn = item.sellIn - 1
     }
 
-    if (items[i].sellIn < 0) {
-      if (AGED_BRIE !== items[i].name) {
-        if (BACKSTAGE !== items[i].name && items[i].quality > 0 && SULFURAS !== items[i].name) {
-          items[i].quality = items[i].quality - 1
+    if (item.sellIn < 0) {
+      if (AGED_BRIE !== item.name) {
+        if (BACKSTAGE !== item.name && item.quality > 0 && SULFURAS !== item.name) {
+          item.quality = item.quality - 1
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          item.quality = item.quality - item.quality
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
         }
 
-        if (AGED_BRIE === items[i].name && items[i].sellIn <= 0) {
-          items[i].quality = 0
+        if (AGED_BRIE === item.name && item.sellIn <= 0) {
+          item.quality = 0
         }
       }
     }
 
-    if (SULFURAS !== items[i].name && items[i].quality > 50) {
-      items[i].quality = 50
+    if (SULFURAS !== item.name && item.quality > 50) {
+      item.quality = 50
     }
   }
 
