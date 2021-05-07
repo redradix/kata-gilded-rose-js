894c69b190d49c4ec34a068cc2ea224c551e150e
Victor Miguez <victor@redradix.com>
2021-05-07 15:55:32 +0200

    Extract hardcoded product names to constants

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,40 +1,47 @@
 import Item from './Item'
 
+const AGED_BRIE = "Aged Brie"
+const DEXTERITY = "+5 Dexterity Vest"
+const ELIXIR = "Elixir of the Mongoose"
+const SULFURAS = "Sulfuras, Hand of Ragnaros"
+const BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+const CONJURED = "Conjured Mana Cake"
+
 var GildedRose = function () {
   var items = []
-  items.push(new Item("+5 Dexterity Vest", 10, 20))
-  items.push(new Item("Aged Brie", 2, 0))
-  items.push(new Item("Elixir of the Mongoose", 5, 7))
-  items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80))
-  items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20))
-  items.push(new Item("Conjured Mana Cake", 3, 6))
+  items.push(new Item(DEXTERITY, 10, 20))
+  items.push(new Item(AGED_BRIE, 2, 0))
+  items.push(new Item(ELIXIR, 5, 7))
+  items.push(new Item(SULFURAS, 0, 80))
+  items.push(new Item(BACKSTAGE, 15, 20))
+  items.push(new Item(CONJURED, 3, 6))
   updateQuality(items)
 }
 
 export function updateQuality(items) {
   for (var i = 0; i < items.length; i++) {
-    if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
+    if (AGED_BRIE != items[i].name && BACKSTAGE != items[i].name) {
       //TODO: Improve this code.
       if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
+        if (SULFURAS != items[i].name) {
           items[i].quality = items[i].quality - 1
         }
       }
     } else {
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
-        if ("Aged Brie" == items[i].name) {
+        if (AGED_BRIE == items[i].name) {
             if (items[i].sellIn < 6) {
               items[i].quality = items[i].quality + 1
             }
         }
         //Increases the Quality of the stinky cheese if its 11 days to due date.
-        if ("Aged Brie" == items[i].name) {
+        if (AGED_BRIE == items[i].name) {
             if (items[i].sellIn < 11) {
               items[i].quality = items[i].quality + 1
             }
         }
-        if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
+        if (BACKSTAGE == items[i].name) {
           if (items[i].sellIn < 11) {
             // See revision number 2394 on SVN.
             if (items[i].quality < 50) {
@@ -50,14 +57,14 @@ export function updateQuality(items) {
         }
       }
     }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
+    if (SULFURAS != items[i].name) {
       items[i].sellIn = items[i].sellIn - 1
     }
     if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
+      if (AGED_BRIE != items[i].name) {
+        if (BACKSTAGE != items[i].name) {
           if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
+            if (SULFURAS != items[i].name) {
               items[i].quality = items[i].quality - 1
             }
           }
@@ -69,11 +76,11 @@ export function updateQuality(items) {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1
         }
-        if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
+        if (AGED_BRIE == items[i].name && items[i].sellIn <= 0)
             items[i].quality = 0
       } // of for.
     }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name)
+    if (SULFURAS != items[i].name)
       if (items[i].quality > 50) items[i].quality = 50
   }
   return items
