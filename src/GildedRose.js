f858bf7768c381de9dd876f0f0e24c6e24ba5bc9
Victor Miguez <victor@redradix.com>
2021-05-07 16:55:29 +0200

    Split and reorganize ifs

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -62,9 +62,12 @@ export function updateQuality(items) {
       if (item.quality < 50) {
         item.quality = item.quality + 1
       }
+      if (item.sellIn < 0 && item.quality > 0) {
+        item.quality = item.quality - item.quality
+      }
     }
 
-    if ([SULFURAS, BACKSTAGE].includes(item.name)) {
+    if (BACKSTAGE === item.name) {
       if (item.sellIn < 0 && item.quality > 0) {
         item.quality = item.quality - item.quality
       }
