d56fda0361f9daa078c293e96e6c2fd5bf75b21b
Ester Legaz <ester@redradix.com>
2022-11-04 12:50:59 +0100

    split backstage

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -34,9 +34,23 @@ export function updateQuality(items) {
       }
     }
 
-    if (AGED_BRIE === item.name || BACKSTAGE === item.name) {
+    if (AGED_BRIE === item.name) {
       if (item.quality < 50) {
         item.quality = increaseQuality(item);
+
+        if (item.sellIn < 6) {
+          item.quality = increaseQuality(item);
+        }
+        if (item.sellIn < 11) {
+          item.quality = increaseQuality(item);
+        }
+      }
+    }
+
+    if (BACKSTAGE === item.name) {
+      if (item.quality < 50) {
+        item.quality = increaseQuality(item);
+
         if (item.sellIn < 6) {
           item.quality = increaseQuality(item);
         }
